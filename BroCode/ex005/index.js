ad2251bdf2480a9059e67d6bfec46ee32d074fa0
Júlio César Landim <127707529+devjuliolandim@users.noreply.github.com>
2024-02-03 18:25:38 -0300

    exercises

---
+++ BroCode/ex005/index.js
@@ -0,0 +1,55 @@
+/*let students = ["julio", "matheus"]
+
+function capitalize(element, index, array){
+    array[index] = element[0].toUpperCase() + element.substring(1)
+}
+
+students.forEach(capitalize)
+
+function print(element){
+
+}*/
+
+
+
+
+// A diferença entre map() e forEach() é que map retorna um array já o forEach() não
+
+
+/*let nums = [1,2,3]
+
+
+let squares = nums.map(square)
+
+print(squares)
+
+function square(element){
+    return Math.pow(element,2)
+}
+
+function cube(element){
+    return Math.pow(element, 3)
+}
+
+function print(element){
+    console.log(element)
+}*/
+
+
+/*let ages = [1,2,54,20,13,45,18,19,23]
+
+let adults = ages.filter(checkAge)
+
+function checkAge(element){
+    return element>=18
+}
+
+function print(element){
+    console.log(element)
+}
+
+
+print(adults)
+*/
+
+const greetings
